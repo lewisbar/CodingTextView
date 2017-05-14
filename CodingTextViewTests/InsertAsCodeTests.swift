276a902210ca2629e27fa433cb730441ae203684
Lennart Wisbar <lewisbar.git@gmail.com>
2017-05-14 23:00:09 +0200

    Continued tests.

--- CodingTextViewTests/InsertAsCodeTests.swift
+++ CodingTextViewTests/InsertAsCodeTests.swift
@@ -42,90 +42,109 @@ class InsertAsCodeUnitTests: XCTestCase {
         textView.text = "Hello"
         cursorOffsetFromEnd = -1
         textView.insertAsCode("a")
+        
         XCTAssertEqual(textView.text, "Hellao")     // "a" after "Hell"
         XCTAssertEqual(cursorOffsetFromEnd, -1)     // after the typed "a"
     }
     
     // MARK: - Colon
     func testColonGivesCaseIndentationOfLastSwitch() {
-        textView.text = "Line 1\n" +
-                        "\t\tswitch test {\n" +
-                        "\t\t\tcase\n" +
-                        "\t\t}"
+        textView.text = "\t\tswitch test {" +
+                        "\n\t\t\tcase" +
+                        "\n\t\t}"
         cursorOffsetFromEnd = -4
         textView.insertAsCode(":")
+        
         XCTAssertEqual(textView.text,
-                       "Line 1\n" +
-                        "\t\tswitch test {\n" +
-                        "\t\tcase:\n" +     // Colon after "case", one tab removed
-                        "\t\t}")
+                        "\t\tswitch test {" +
+                        "\n\t\tcase:" +     // Colon after "case", one tab removed
+                        "\n\t\t}")
         XCTAssertEqual(cursorOffsetFromEnd, -4) // after the typed colon
     }
     
     func testColonGivesDefaultIndentationOfLastSwitch() {
-        textView.text = "Line 1\n" +
-                        "\t\tswitch test {\n" +
-                        "\t\t\tdefault\n" +
-                        "\t\t}"
+        textView.text = "\t\tswitch test {" +
+                        "\n\t\t\tdefault" +
+                        "\n\t\t}"
         cursorOffsetFromEnd = -4
         textView.insertAsCode(":")
+        
         XCTAssertEqual(textView.text,
-                       "Line 1\n" +
-                        "\t\tswitch test {\n" +
-                        "\t\tdefault:\n" +     // Colon after "default", one tab removed
-                        "\t\t}")
+                        "\t\tswitch test {" +
+                        "\n\t\tdefault:" +     // Colon after "default", one tab removed
+                        "\n\t\t}")
         XCTAssertEqual(cursorOffsetFromEnd, -4) // after the typed colon
     }
     
     func testColonTreatedNormallyIfNoSwitch() {
-        textView.text = "Line 1\n" +
-                        "\t\t\tcase\n" +
-                        "\t\t}"
+        textView.text = "\t\t\tcase" +
+                        "\n\t\t}"
         cursorOffsetFromEnd = -4
         textView.insertAsCode(":")
+        
         XCTAssertEqual(textView.text,
-                       "Line 1\n" +
-                        "\t\t\tcase:\n" +     // Colon after "case", no tabs removed
-                        "\t\t}")
+                        "\t\t\tcase:" +     // Colon after "case", no tabs removed
+                        "\n\t\t}")
         XCTAssertEqual(cursorOffsetFromEnd, -4) // after the typed colon
     }
     
     func testColonTreatedNormallyIfNoCaseOrDefault() {
-        textView.text = "Line 1\n" +
-                        "\t\t\tnormalText"
+        textView.text = "\t\t\tnormalText"
         cursorOffsetFromEnd = 0
         textView.insertAsCode(":")
+        
         XCTAssertEqual(textView.text,
-                       "Line 1\n" +
-                        "\t\t\tnormalText:")    // Colon after "normalText", no tabs removed
+                       "\t\t\tnormalText:")    // Colon after "normalText", no tabs removed
         XCTAssertEqual(cursorOffsetFromEnd, 0)  // at the end
     }
     
     // MARK: - Return Key
-    func testReturnKeyMaintainsIndentationAfterNormalCharacter() {
-        textView.text = "Line 1\n" +
-                        "\t\tnormalText"
+    func testReturnKeyAfterNormalCharacterMaintainsIndentation() {
+        textView.text = "\t\tnormalText"
         cursorOffsetFromEnd = 0
         textView.insertAsCode("\n")
+        
         XCTAssertEqual(textView.text,
-                       "Line 1\n" +
-                        "\t\tnormalText\n" +
-                        "\t\t")     // Indentation level maintained
+                        "\t\tnormalText" +
+                        "\n\t\t")     // Indentation level maintained
         XCTAssertEqual(cursorOffsetFromEnd, 0)  // at the end
     }
     
-    func testReturnKeyAfterSwitchWithCurlyBraceMaintainsIndentationAndClosesBrace() {
-        textView.text = "Line 1\n" +
-                        "\t\tswitch test {"
+    func testReturnKeyAfterCurlyBraceIndentsNextLineAndClosesBrace() {
+        textView.text = "\t\ttest {"
         cursorOffsetFromEnd = 0
         textView.insertAsCode("\n")
+        
         XCTAssertEqual(textView.text,
-                       "Line 1\n" +
-                        "\t\tswitch test {\n" +
-                        "\t\t\n" +    // Indentation level maintained
-                        "\t\t}")     // Closed curly brace added
-        XCTAssertEqual(cursorOffsetFromEnd, -4) // in the line after the switch, after the indentation
+                        "\t\ttest {" +
+                        "\n\t\t\t" +    // Indentation level maintained
+                        "\n\t\t}")     // Closed curly brace added
+        XCTAssertEqual(cursorOffsetFromEnd, -4)
     }
     
+    func testReturnKeyBetweenCurlyBracesDoesNotAddAnotherBrace() {
+        textView.text = "\t\ttest {}"
+        cursorOffsetFromEnd = -1
+        textView.insertAsCode("\n")
+        
+        XCTAssertEqual(textView.text,
+                        "\t\ttest {" +
+                        "\n\t\t\t" +    // Indentation level maintained
+                        "\n\t\t}")     // Closed curly brace added
+        XCTAssertEqual(cursorOffsetFromEnd, -4)
+    }
     
+    func testReturnKeyAfterSwitchWithCurlyBraceMaintainsIndentationAndClosesBrace() {
+        textView.text = "\t\tswitch test {"
+        cursorOffsetFromEnd = 0
+        textView.insertAsCode("\n")
+        
+        XCTAssertEqual(textView.text,
+                        "\t\tswitch test {" +
+                        "\n\t\t" +    // Indentation level maintained
+                        "\n\t\t}")     // Closed curly brace added
+        XCTAssertEqual(cursorOffsetFromEnd, -4) // end of the middle line
+    }
+    
+
 }
