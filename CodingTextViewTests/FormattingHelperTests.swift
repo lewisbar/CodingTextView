5098eff639e51d25ba9a29f9c0576ca3619fd677
Lennart Wisbar <lewisbar.git@gmail.com>
2017-05-16 07:38:13 +0200

    Continued tests.

--- CodingTextViewTests/FormattingHelperTests.swift
+++ CodingTextViewTests/FormattingHelperTests.swift
@@ -12,7 +12,7 @@
 import XCTest
 @testable import CodingTextView
 
-class InsertAsCodeUnitTests: XCTestCase {
+class FormattingHelperUnitTests: XCTestCase {
     
     let textView = UITextView()
     
@@ -43,7 +43,7 @@ class InsertAsCodeUnitTests: XCTestCase {
     // MARK: - Normal Characters
     func test_NormalCharacters_NotAffected() {
         textView.text = "Hello"
-        cursorOffsetFromEnd = -1
+        cursorOffsetFromEnd = -1    // Before the "o"
         textView.insertAsCode("a")
         
         XCTAssertEqual(textView.text, "Hellao")     // "a" after "Hell"
@@ -55,7 +55,7 @@ class InsertAsCodeUnitTests: XCTestCase {
         textView.text = "\t\tswitch test {" +
                         "\n\t\t\tcase" +
                         "\n\t\t}"
-        cursorOffsetFromEnd = -4
+        cursorOffsetFromEnd = -4    // After the open curly brace
         textView.insertAsCode(":")
         
         XCTAssertEqual(textView.text,
@@ -69,26 +69,23 @@ class InsertAsCodeUnitTests: XCTestCase {
         textView.text = "\t\tswitch test {" +
                         "\n\t\t\tdefault" +
                         "\n\t\t}"
-        cursorOffsetFromEnd = -4
+        cursorOffsetFromEnd = -4    // After the open curly brace
         textView.insertAsCode(":")
         
         XCTAssertEqual(textView.text,
                         "\t\tswitch test {" +
                         "\n\t\tdefault:" +     // Colon after "default", one tab removed
                         "\n\t\t}")
-        XCTAssertEqual(cursorOffsetFromEnd, -4) // after the typed colon
+        XCTAssertEqual(cursorOffsetFromEnd, -4) // After the typed colon
     }
     
     func test_ColonAfterCase_TreatedNormally_IfNoSwitch() {
-        textView.text = "\t\t\tcase" +
-                        "\n\t\t}"
-        cursorOffsetFromEnd = -4
+        textView.text = "\t\t\tcase"
+        cursorOffsetFromEnd = 0
         textView.insertAsCode(":")
         
-        XCTAssertEqual(textView.text,
-                        "\t\t\tcase:" +     // Colon after "case", no tabs removed
-                        "\n\t\t}")
-        XCTAssertEqual(cursorOffsetFromEnd, -4) // after the typed colon
+        XCTAssertEqual(textView.text, "\t\t\tcase:")    // No tabs removed
+        XCTAssertEqual(cursorOffsetFromEnd, 0)
     }
     
     func test_Colon_TreatedNormally_IfNoCaseOrDefault() {
@@ -97,8 +94,8 @@ class InsertAsCodeUnitTests: XCTestCase {
         textView.insertAsCode(":")
         
         XCTAssertEqual(textView.text,
-                       "\t\t\tnormalText:")    // Colon after "normalText", no tabs removed
-        XCTAssertEqual(cursorOffsetFromEnd, 0)  // at the end
+                       "\t\t\tnormalText:")    // No tabs removed
+        XCTAssertEqual(cursorOffsetFromEnd, 0)
     }
     
     // MARK: - Return Key
@@ -110,7 +107,7 @@ class InsertAsCodeUnitTests: XCTestCase {
         XCTAssertEqual(textView.text,
                         "\t\tnormalText" +
                         "\n\t\t")     // Indentation level maintained
-        XCTAssertEqual(cursorOffsetFromEnd, 0)  // at the end
+        XCTAssertEqual(cursorOffsetFromEnd, 0)
     }
     
     func test_ReturnKeyAfterCurlyBrace_IndentsNextLineAndClosesBrace() {
@@ -122,19 +119,32 @@ class InsertAsCodeUnitTests: XCTestCase {
                         "\t\ttest {" +
                         "\n\t\t\t" +    // Indentation level maintained
                         "\n\t\t}")     // Closed curly brace added
-        XCTAssertEqual(cursorOffsetFromEnd, -4)
+        XCTAssertEqual(cursorOffsetFromEnd, -4) // End of the middle line
     }
     
-    func test_ReturnKeyBetweenCurlyBraces_DoesNotAddAnotherBrace() {
+    func test_ReturnKeyBetweenCurlyBraces_UsesExistingBrace() {
         textView.text = "\t\ttest {}"
-        cursorOffsetFromEnd = -1
+        cursorOffsetFromEnd = -1    // Between the {}
         textView.insertAsCode("\n")
         
         XCTAssertEqual(textView.text,
                         "\t\ttest {" +
                         "\n\t\t\t" +    // Indentation level maintained
-                        "\n\t\t}")     // Closed curly brace added
-        XCTAssertEqual(cursorOffsetFromEnd, -4)
+                        "\n\t\t}")      // No curly brace added
+        XCTAssertEqual(cursorOffsetFromEnd, -4) // End of the middle line
+    }
+    
+    func test_ReturnKeyAfterCurlyBrace_DoesNotAddAnotherBrace_IfTooManyClosedBraces() {
+        textView.text = "test {" +
+                        "\nanother line }"
+        cursorOffsetFromEnd = -15   // After the open curly brace
+        textView.insertAsCode("\n")
+        
+        XCTAssertEqual(textView.text,
+                        "test {" +
+                        "\n\t" +    // Indentation works as normal after "{"
+                        "\nanother line }") // No extra "}" added
+        XCTAssertEqual(cursorOffsetFromEnd, -15)    // End of the middle line
     }
     
     func test_ReturnKeyAfterSwitchWithCurlyBrace_MaintainsIndentationAndClosesBrace() {
@@ -146,8 +156,78 @@ class InsertAsCodeUnitTests: XCTestCase {
                         "\t\tswitch test {" +
                         "\n\t\t" +    // Indentation level maintained
                         "\n\t\t}")     // Closed curly brace added
-        XCTAssertEqual(cursorOffsetFromEnd, -4) // end of the middle line
+        XCTAssertEqual(cursorOffsetFromEnd, -4) // End of the middle line
     }
     
-
+    func test_ReturnKeyAfterCaseWithColon_IndentsNextLine() {
+        textView.text = "\t\tcase test:"
+        cursorOffsetFromEnd = 0
+        textView.insertAsCode("\n")
+        
+        XCTAssertEqual(textView.text,
+                        "\t\tcase test:" +
+                        "\n\t\t\t")     // Indentation level raised
+        XCTAssertEqual(cursorOffsetFromEnd, 0) // End of new line
+    }
+    
+    func test_ReturnKeyAfterCaseWithColonAndSpaces_IndentsNextLine() {
+        textView.text = "\t\tcase test:   "
+        cursorOffsetFromEnd = 0
+        textView.insertAsCode("\n")
+        
+        XCTAssertEqual(textView.text,
+                        "\t\tcase test:   " +
+                        "\n\t\t\t")     // Indentation level raised
+        XCTAssertEqual(cursorOffsetFromEnd, 0) // End of new line
+    }
+    
+    func test_ReturnKeyAfterCaseWithoutColon_DoesNotIndentNextLine() {
+        textView.text = "\t\tcase test"
+        cursorOffsetFromEnd = 0
+        textView.insertAsCode("\n")
+        
+        XCTAssertEqual(textView.text,
+                        "\t\tcase test" +
+                        "\n\t\t")     // Indentation level maintained
+        XCTAssertEqual(cursorOffsetFromEnd, 0) // End of new line
+    }
+    
+    // MARK: - Open Brackets
+    func test_openRoundBracket_ProducesClosedBracket() {
+        textView.text = "test"
+        cursorOffsetFromEnd = 0
+        textView.insertAsCode("(")
+        
+        XCTAssertEqual(textView.text, "test()") // Bracket closed
+        XCTAssertEqual(cursorOffsetFromEnd, -1) // Between the brackets
+    }
+    
+    func test_openSquareBracket_ProducesClosedBracket() {
+        textView.text = "test"
+        cursorOffsetFromEnd = 0
+        textView.insertAsCode("[")
+        
+        XCTAssertEqual(textView.text, "test[]") // Bracket closed
+        XCTAssertEqual(cursorOffsetFromEnd, -1) // Between the brackets
+    }
+    
+    func test_openRoundBracket_DoesNotProduceClosedBracket_IfTooManyClosedBrackets() {
+        textView.text = "bracket test)"
+        cursorOffsetFromEnd = -5    // Before "test"
+        textView.insertAsCode("(")
+        
+        XCTAssertEqual(textView.text, "bracket (test)") // No additional bracket
+        XCTAssertEqual(cursorOffsetFromEnd, -5) // After the open bracket
+    }
+    
+    func test_openSquareBracket_DoesNotProduceClosedBracket_IfTooManyClosedBrackets() {
+        textView.text = "bracket test]"
+        cursorOffsetFromEnd = -5    // Before "test"
+        textView.insertAsCode("[")
+        
+        XCTAssertEqual(textView.text, "bracket [test]") // No additional bracket
+        XCTAssertEqual(cursorOffsetFromEnd, -5) // After the open bracket
+    }
+    
+    
 }
